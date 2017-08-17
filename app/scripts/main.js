53c4c0f53f870b02dc0e182044e8236f702799bd
Malene <maleneklit@hotmail.com>
2017-08-17 16:59:31 +0200

    make setup for jQuery

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
 console.log('\'Allo \'Allo!');
+// self made swiper
+
+$('.swiper-button-next').click(function() {
+  alert( "Handler for .click() called." );
+});
+
+$('.swiper-button-prev').click(function() {
+  alert( "Handler for .click() called." );
+});
